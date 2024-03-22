adb8c24d9d1fc16b88600fcb35e36862e5193954
Mariana Vilela <mariana.marianavilela@gmail.com>
2024-03-22 01:30:03 -0300

    comeco atividade 08

---
+++ pratica03/src/Atividade08.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Atividade08 {
+
+    public static void main(String[] args) {
+        try (Scanner console = new Scanner(System.in)) {
+
+          System.out.print("Informe os valores de a, b e c: ");
+          String equacao = console.next();
+
+          double a = 0;
+          double b = 0;
+          double c = 0;
+
+          if (()) {
+              
+        }
+      }
+    }
+    //Funcionando
